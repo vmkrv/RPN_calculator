df442a52208cf99643d1285d3ae701469586885b
Veaceslav Macarov <veaceslavmacarov@MacBook-Pro-Veaceslav.local>
2015-09-07 20:04:13 +0500

    Initial Commit

---
+++ RPN_Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RPN_Calculator
+//
+//  Created by Veaceslav Macarov on 07.09.15.
+//  Copyright (c) 2015 Veaceslav Macarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RPN_Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RPN_Calculator
+//
+//  Created by Veaceslav Macarov on 07.09.15.
+//  Copyright (c) 2015 Veaceslav Macarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
